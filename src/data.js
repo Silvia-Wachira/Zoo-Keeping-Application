feb12041cef51d43e0c0c3d17b40b64d6f3f96a0
Silvia-Wachira <wachirasilvia8@gmail.com>
2023-07-06 10:51:30 +0300

    create data in data.js

--- src/data.js
+++ src/data.js
@@ -1,28 +1,30 @@
 {
-    "animals": [
+    const animalData = [
       {
-        "id": 1,
-        "name": "Pangolia",
+        "name": "Red Panda",
         "imageUrl": "",
-        "description": "",
+        "description": "https://images.pexels.com/photos/145902/pexels-photo-145902.jpeg?auto=compress&cs=tinysrgb&w=400",
         "donations": 0
       },
       {
-        "id": 2,
+        "name": "Pangolia",
+        "imageUrl": "https://media.istockphoto.com/id/675014092/photo/pangolin.jpg?b=1&s=612x612&w=0&k=20&c=P_5immI0w-woxHqq5W0hWlUsXNblnxqtu2wyW72o0zY=",
+        "description": "Pangolins, sometimes known as scaly anteaters, are mammals of the order Pholidota",
+        "donations": 0
+      },
+      {
         "name": "Mantis Shrimp",
-        "imageUrl": "https://media.istockphoto.com/id/1276370504/photo/indian-pangolin-or-anteater-one-of-the-most-traffic-wildlife-species.jpg?b=1&s=612x612&w=0&k=20&c=WQ9tA19Lc2PTyfQ4nrYW0nsbvGwnwX2orOqdZBN0Agw=",
-        "description": "Pangolins, sometimes known as scaly anteaters, are mammals of the order Pholidota ",
+        "imageUrl": "https://images.pexels.com/photos/1085542/pexels-photo-1085542.jpeg?auto=compress&cs=tinysrgb&w=400",
+        "description": "Mantis shrimp are carnivorous marine crustaceans of the order Stomatopoda.",
         "donations": 0
       },
       {
-        "id": 3,
         "name": "Gibbon",
         "imageUrl": "https://images.pexels.com/photos/7709934/pexels-photo-7709934.jpeg?auto=compress&cs=tinysrgb&w=400",
         "description": "Gibbons are apes in the family Hylobatidae. The family historically contained one genus, but now is split into four extant genera and 20 species.",
         "donations": 0
       },
       {
-        "id": 4,
         "name": "Wombat",
         "imageUrl": "https://images.pexels.com/photos/13008971/pexels-photo-13008971.jpeg?auto=compress&cs=tinysrgb&w=400",
         "description": "Wombats are short-legged, muscular quadrupedal marsupials of the family Vombatidae that are native to Australia.",
@@ -30,3 +32,4 @@
       }
     ]
   }
+  console.log('bottom of data')
